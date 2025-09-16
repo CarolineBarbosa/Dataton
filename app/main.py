c548e3fa88d7eda2a74bd95a65ef038073e9f423
Laidone <laidonecarvalho@hotmail.com>
2025-09-15 23:03:36 -0300

    Grafana+Prometheus

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+from starlette.responses import Response
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"mensagem": "API rodando 🚀"}
+
+# Contador de requisições
+REQUEST_COUNT = Counter("requests_total", "Total de requisições ao modelo")
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type=CONTENT_TYPE_LATEST)
+
+@app.get("/predict")
+def predict():
+    REQUEST_COUNT.inc()
+    # lógica do modelo
+    return {"prediction": "exemplo"}
