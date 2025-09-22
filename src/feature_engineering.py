29695ed05f4e91293aa41b562672c0101ee32e7e
Caroline Cunha Barbosa <barbosa.caroline@ifood.com.br>
2025-09-21 22:08:29 -0300

    Add data ingestion functionality and datasource configuration
    - Implemented data ingestion from JSON to Parquet format in feature_engineering.py.
    - Created datasource_config.py to manage datasource configurations from a YAML file.
    - Added utility functions for loading JSON and YAML files in utils/__init__.py.
    - Introduced data_source_config.yaml for defining data sources.
    - Updated .gitignore to include JSON and Parquet file extensions.

---
+++ src/feature_engineering.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from utils import load_json, save_to_parquet
+
+def ingest_data(file_config) -> None:
+    """
+    Ingest data from a JSON file and convert it to a pandas DataFrame.
+
+    :param file_path: Path to the JSON file
+    :param id_column_name: Name of the ID column in the DataFrame
+    :return: pandas DataFrame containing the ingested data
+    """
+    df = load_json(file_config)
+    name = f"{file_config['path'].split('/')[-1].replace('.json', '.parquet')}"
+    save_to_parquet(df, name)
+    return 
+
+def run_ingestion_pipeline(datasource_config) -> None:
+    """
+    Run the ingestion pipeline to convert JSON data to Parquet format for predefined datasources.
+
+    :param input_paths: Dictionary mapping datasource names to their respective file paths
+    """
+
+    for entity, entity_config in datasource_config.items():
+        print(f"Ingesting data for entity: {entity}")
+        print(f"Path: {entity_config['path']}")
+        ingest_data(entity_config)
+
+
