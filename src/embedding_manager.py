b7b1644ce47692e4c337e8fa7b3fd9a2da313119
Caroline Cunha Barbosa <barbosa.caroline@ifood.com.br>
2025-09-22 23:19:20 -0300

    fix: update paths in index configuration and refactor embedding model integration

--- src/embedding_manager.py
+++ src/embedding_manager.py
@@ -1,6 +1,8 @@
 from typing import List, Union
 import numpy as np
-from src.feature_engineering import EmbeddingModel
+from sentence_transformers import SentenceTransformer
+import yaml
+
 
 class EmbeddingManager:
     """
@@ -19,3 +21,30 @@ class EmbeddingManager:
             return np.asarray(vecs, dtype=np.float32)
         else:
             raise TypeError("text must be str or list[str]")
+        
+
+class EmbeddingModel:
+    def __init__(self, config_path):
+
+        with open(config_path, 'r') as file:
+            config = yaml.safe_load(file)
+        self.model_name = config['embedding_model']['name']
+        self.model = SentenceTransformer(self.model_name)
+
+    def encode(self, texts, normalize_embeddings=True):
+        return self.model.encode(texts, normalize_embeddings=normalize_embeddings)
+
+
+# def generate_embeddings(df, text_column, model_config_path='models_config.yaml'):
+#     """Generate embeddings for the text column using a pre-trained model."""
+#     embedding_model = EmbeddingModel(config_path=model_config_path)
+#     df['embeddings'] = list(embedding_model.encode(
+#         df[text_column].tolist(), 
+#         normalize_embeddings=True
+#     ))
+#     embeddings = embedding_model.encode(
+#         df[text_column].tolist(), 
+#         normalize_embeddings=True
+#     )
+#     df['embeddings'] = embeddings
+#     return df
--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,29 +1,13 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
-import re
-from pydantic import BaseModel
 from utils import load_parquet
-import yaml
-
-class EmbeddingModel:
-    def __init__(self, config_path):
-        with open(config_path, 'r') as file:
-            config = yaml.safe_load(file)
-        self.model_name = config['embedding_model']['name']
-        self.model = SentenceTransformer(self.model_name)
-
-    def encode(self, texts, normalize_embeddings=True):
-        return self.model.encode(texts, normalize_embeddings=normalize_embeddings)
+import numpy as np
+import pandas as pd
 
 
 def combine_columns(df, columns_to_combine):
     """Combine specified columns into a single text column."""
-    df['text'] = df[columns_to_combine].fillna('').apply(
+    df['text'] = df[columns_to_combine].apply(
         lambda row: ' '.join([
-            f"{col}: {row[col]}" for col in columns_to_combine
+            f"{col}: {row[col]}" for col in columns_to_combine if pd.notnull(row[col])
         ]),
         axis=1
     )
@@ -33,24 +17,10 @@ def filter_columns(df, columns_to_keep):
     """Filter the dataframe to keep only the specified columns."""
     return df[columns_to_keep]
 
-def generate_embeddings(df, text_column, model_config_path='models_config.yaml'):
-    """Generate embeddings for the text column using a pre-trained model."""
-    embedding_model = EmbeddingModel(config_path=model_config_path)
-    df['embeddings'] = list(embedding_model.encode(
-        df[text_column].tolist(), 
-        normalize_embeddings=True
-    ))
-    embeddings = embedding_model.encode(
-        df[text_column].tolist(), 
-        normalize_embeddings=True
-    )
-    df['embeddings'] = embeddings
-    return df
 
 def process_entity(file_path, columns_to_combine, columns_to_keep):
     """Process an entity by loading data, combining columns, and generating embeddings."""
     df = load_parquet(file_path)
     df = combine_columns(df, columns_to_combine)
     df = filter_columns(df, columns_to_keep)
-    df = generate_embeddings(df, 'text')
     return df
